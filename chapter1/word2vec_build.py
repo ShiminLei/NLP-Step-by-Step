a51be2238623d14e55440dc136312854ddd16300
ShiminLei <sl2kd@virginia.edu>
2020-04-25 17:31:42 -0400

    new

---
+++ chapter1/word2vec_build.py
@@ -0,0 +1,80 @@
+import os
+import pickle
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+from gensim.models.keyedvectors import KeyedVectors
+
+
+def dump_pkl(vocab, pkl_path, overwrite=True):
+    """
+    存储文件
+    :param pkl_path:
+    :param overwrite:
+    :return:
+    """
+    if pkl_path and os.path.exists(pkl_path) and not overwrite:
+        return
+    if pkl_path:
+        with open(pkl_path, 'wb') as f:
+            pickle.dump(vocab, f, protocol=pickle.HIGHEST_PROTOCOL)
+            # pickle.dump(vocab, f, protocol=0)
+        print("save %s ok." % pkl_path)
+
+
+def read_lines(path, col_sep=None):
+    lines = []
+    with open(path, mode='r', encoding='utf-8') as f:
+        for line in f:
+            line = line.strip()
+            if col_sep:
+                if col_sep in line:
+                    lines.append(line)
+            else:
+                lines.append(line)
+    return lines
+
+
+def extract_sentence(train_x_seg_path, train_y_seg_path, test_seg_path):
+    ret = []
+    lines = read_lines(train_x_seg_path)
+    lines += read_lines(train_y_seg_path)
+    lines += read_lines(test_seg_path)
+    for line in lines:
+        ret.append(line)
+    return ret
+
+
+def save_sentence(lines, sentence_path):
+    with open(sentence_path, 'w', encoding='utf-8') as f:
+        for line in lines:
+            f.write('%s\n' % line.strip())
+    print('save sentence:%s' % sentence_path)
+
+
+def build(train_x_seg_path, test_y_seg_path, test_seg_path, out_path=None, sentence_path='',
+          w2v_bin_path="w2v.bin", min_count=100):
+    sentences = extract_sentence(train_x_seg_path, test_y_seg_path, test_seg_path)
+    save_sentence(sentences, sentence_path)
+    print('train w2v model...')
+    # train model
+    w2v = Word2Vec(sg=1, sentences=LineSentence(sentence_path),
+                   size=256, window=5, min_count=min_count, iter=5)
+    w2v.wv.save_word2vec_format(w2v_bin_path, binary=True)
+    print("save %s ok." % w2v_bin_path)
+    # test
+    sim = w2v.wv.similarity('技师', '车主')
+    print('技师 vs 车主 similarity score:', sim)
+    # load model
+    model = KeyedVectors.load_word2vec_format(w2v_bin_path, binary=True)
+    word_dict = {}
+    for word in model.vocab:
+        word_dict[word] = model[word]
+    dump_pkl(word_dict, out_path, overwrite=True)
+
+
+if __name__ == '__main__':
+    build('data/train_set.seg_x.txt',
+          'data/train_set.seg_y.txt',
+          'data/test_set.seg_x.txt',
+          out_path='data/word2vec.txt',
+          sentence_path='data/sentences.txt',)
---
+++ chapter1/workshop_test1.py
@@ -0,0 +1,86 @@
+import numpy as np
+import pandas as pd
+import re
+from jieba import posseg
+import jieba
+import os
+import time
+import gensim
+from gensim.models import Word2Vec
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+from gensim.models.word2vec import LineSentence
+
+"""
+汉语分词
+"""
+def segment(sentence, cut_type='word', pos=False):
+    if pos: # 如果需要词性
+        if cut_type == 'word':
+            word_pos_seq = posseg.lcut(sentence)
+            word_seq, pos_seq = [], []
+            for w, p in word_pos_seq:  # p代表的是词性
+                word_seq.append(w)
+                pos_seq.append(p)
+            return word_seq, pos_seq
+        elif cut_type == 'char':
+            word_seq = list(sentence)
+            pos_seq = []
+            for w in word_seq:
+                w_p = posseg.lcut(w)
+                pos_seq.append(w_p[0].flag)
+            return word_seq, pos_seq
+    else:
+        if cut_type == 'word':
+            return jieba.lcut(sentence)
+        elif cut_type == 'char':
+            return list(sentence)
+print(segment('我比较喜欢成都的生活', 'word'))
+print(segment('我比较喜欢成都的生活', 'char'))
+print(segment('我比较喜欢成都的生活', 'word', True))
+print(segment('我比较喜欢成都的生活', 'char', True))
+
+
+# Cutting words and saving to corpus file
+def write_token_to_file(infile, outfile):
+    words = []
+    for line in open(infile, 'r', encoding='utf-8'):
+        line = line.strip()
+        if line:
+            w = jieba.lcut(line)
+            words += w + ['\n']
+    outfile.writelines(' '.join(words))
+
+
+def train_w2v_model(data_path, model_path):
+    start_time = time.time()
+    w2v_model = Word2Vec(sentences=LineSentence(data_path), workers=4, size=50, min_count=5)  # Using 4 threads min_count = 5
+    w2v_model.save(model_path) # Can be used for continue training
+    # w2v_model.wv.save(model_path) # Smaller and faster but can't be trained later
+    print('training time:', time.time() - start_time)
+
+
+def get_model_from_file():
+    model = Word2Vec.load('w2v.model')
+    return model
+
+
+if __name__ == '__main__':
+
+    data_path = './data/small_corpus.txt'
+    model_path = './model/w2v.model'
+    train_w2v_model(data_path, model_path)
+    model = Word2Vec.load(model_path)
+    print(model.most_similar('车'))
+
+    with open('./data/sentences.txt', 'r', encoding='utf-8') as f:
+        data = f.readlines()
+    new_words = []
+    for line in data:
+        line = line.strip().split(' ')
+        new_words.append(line)
+    model.train(sentences=new_words, epochs=1, total_examples=len(new_words))  # train的样本有多少个
+    model.save('./model/w2v_add.model')
+    model_add = Word2Vec.load('./model/w2v_add.model')
+    print(model_add.most_similar('车'))
+    print(model_add['的基督教基督教'])
